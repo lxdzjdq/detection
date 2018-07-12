dbc29124dca0810daa38f437c23eba18c5baac34
lixuedong <824998340@qq.com>
2018-07-13 00:16:35 +0800

    modify code

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ pipeline_config_path=$output_dir/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
-# rm -rvf $output_dir/*
+rm -rvf $output_dir/*
 
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $dataset_dir/$config $pipeline_config_path
